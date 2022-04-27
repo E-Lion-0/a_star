dd37f1cbc19b7e99bfac3862dbe830a335c412c5
Elion <elion.karaboja@stud.unifi.it>
2022-04-27 16:00:35 +0200

    Adding SFML libraries to the project

--- main.cpp
+++ main.cpp
@@ -8,5 +8,22 @@
 
 int main(){
     std::cout<<"A-Star algorithm!"<<std::endl;
+    sf::RenderWindow window(sf::VideoMode(300,300),"First Window");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
     return 0;
 }
